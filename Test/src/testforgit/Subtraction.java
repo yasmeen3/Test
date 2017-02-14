4c1c8a80d99bd5a38078e9f2a4fa78d9b74c0036
Heba-ElGreatly <heba.elgreatly@gmail.com>
2017-02-14 14:19:45 +0200

    subtraction

---
+++ Test/src/testforgit/Subtraction.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testforgit;
+
+/**
+ *
+ * @author El-Greatly
+ */
+public class Subtraction {
+    float subtract(float a,float b){
+        return a-b;
+    }
+    
+}
